79ea3a845c6eec5b491149e234a04f1e11771883
Sxnny <gr6064@gmail.com>
2023-12-03 17:58:04 +0800

    fix : Adjust Hitzone location

--- src/SceneData.ts
+++ src/SceneData.ts
@@ -70,9 +70,9 @@ const scenesData: SceneData = {
             id: 5,
             background_url: "mountain5.png",
             hitzones: [
-                { x: "10%", y: "50%", goto: 4 },
+                { x: "15%", y: "50%", goto: 4 },
                 { x: "50%", y: "50%", goto: 6 },
-                { x: "90%", y: "50%", goto: 7 },
+                { x: "80%", y: "50%", goto: 7 },
 
             ],
             imageUrl: "https://lh3.googleusercontent.com/pw/ADCreHeAdCNMvLzef-mTtJY0I01kGiOLbkhJn2-mUAgDznXrsuC-EwHB_RsjZfEoMX2sJW7eQEvcaC5sKnYlpF0s_YVYO7noO2YsgmdKRDwF-DYKjMwNC2ina3eGmdZekw7r6tQ_4WIjAO3BszB22_xZ-iHt=w958-h160-s-no-gm?authuser=0",
@@ -81,7 +81,7 @@ const scenesData: SceneData = {
             id: 6,
             background_url: "mountain6.png",
             hitzones: [
-                { x: "10%", y: "50%", goto: 5 },
+                { x: "50%", y: "80%", goto: 5 },
 
             ],
             imageUrl: "https://lh3.googleusercontent.com/pw/ADCreHfORQMJrT8LXU-wF3oNUk3GXlJB6E-GkDVPjxH_Hf8d9C0sACxgWXRZB72REhJP9Gy2oeEp09vax3281y_OteSfGHKKKuGudsdfXdLQOknXRcJu9TuSDpA4kGmlpKi-MsqPkLeMEhn8ijE-KYekNWaW=w958-h153-s-no-gm?authuser=0",
@@ -90,9 +90,9 @@ const scenesData: SceneData = {
             id: 7,
             background_url: "mountain7.png",
             hitzones: [
-                { x: "10%", y: "50%", goto: 5 },
-                { x: "50%", y: "50%", goto: 8 },
-                { x: "90%", y: "50%", goto: 9 },
+                { x: "30%", y: "50%", goto: 5 },
+                { x: "65%", y: "50%", goto: 8 },
+                { x: "83%", y: "50%", goto: 9 },
 
             ],
             imageUrl: "https://lh3.googleusercontent.com/pw/ADCreHfjxFPGKQp5JdZL1ZEloaAF8BqbqhcDHb_4irAdbRBsDv32-NAJBS3XG8ztLcySbOmD2208AeCJxaQMKh7f7iUcs46STSHoR5CaOoYqnCjGaZAviR5Dd5WJYOuytK4Ga_w2VA49PFOVnQUwOf0qYkon=w958-h141-s-no-gm?authuser=0",
@@ -102,7 +102,7 @@ const scenesData: SceneData = {
             background_url: "mountain8.png",
             hitzones: [
                 { x: "10%", y: "50%", goto: 9},
-                { x: "90%", y: "50%", goto: 7},
+                { x: "45%", y: "50%", goto: 7},
 
             ],
             imageUrl: "https://lh3.googleusercontent.com/pw/ADCreHdveMLtUy7VqnVYrik6wOQFB2wxiTjfIEdX7ss6Yyn-OU_jfn3461Ub46xmj_OsEE-NcvJJNpV6v34KeWauAAmNgMEOO-2w78HurNmAS0y3DeJDqTMQYbkx3NUKL4r1Kww4DWe6EC6k9b3mgSHCjxE0=w958-h153-s-no-gm?authuser=0",
